a2f97e7b5672ba3c037fb634a148ddc70325bea9
martin <martinzanandrea09@gmail.com>
2025-10-16 10:39:28 -0300

    ok

--- Api con Express/3.express.js
+++ Api con Express/3.express.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const http = require('http')
+// const http = require('http')
 const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
